5022a7d6b38378d29e22778a521f67c2fcc49eb3
Pablo Moreno Tepichin <pmoretepi@gmail.com>
2017-09-30 21:01:55 -0500

    se renombra el proyecto. Se agregan 3 archivos settings base, production y local

--- src/sistemaIncidencias/wsgi.py
+++ src/sistemaIncidencias/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for incidencias project.
+WSGI config for sistemaIncidencias project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "incidencias.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sistemaIncidencias.settings")
 
 application = get_wsgi_application()
